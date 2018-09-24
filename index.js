b6709dcd67ec0baa60876ae250c2b18bc8b2835d
Robin Kim <rkim1214@gmail.com>
2018-09-24 18:17:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var recipes = {ingredient: "amount"}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   updatedObject = Object.assign({}, object, {[key]: value})
+  return updatedObject;
 }
