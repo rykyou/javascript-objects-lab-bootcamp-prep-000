4a4329b389f8765ef3b31d3e5789a3df1981373e
Robin Kim <rkim1214@gmail.com>
2018-09-24 18:29:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  newObj = Object.assign({}, object)
-  delete newObj.key;
+  var newObj = Object.assign({}, object)
+  delete newObj[key];
   return newObj;
 }
